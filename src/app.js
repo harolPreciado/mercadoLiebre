ffdd29f7d9aba3bced0dea9c98442bd9a134dee6
harolPreciado <harol.andres1999@gmail.com>
2021-10-08 16:09:53 -0500

    Versión inicial mercadoLiebre

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get('/registrar',(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/registrar.html"));
+});
+
+app.get('/login',(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.listen(port, () => console.log(`Server is running in port ${port}`))
